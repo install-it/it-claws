a40949e57dcf360347f3bcf4f1ba21701a23e2a6
markmybytes <markmybytes@gmail.com>
2025-11-10 10:04:02 +0800

    Remove redundant symlink creation

--- scripts/entry_point.sh
+++ scripts/entry_point.sh
@@ -6,14 +6,14 @@ mkdir -p /config/app /config/rclone
 
 if [ ! -L /app/config ] && [ -d /app/config ]; then
     rm -rf /app/config
+    ln -sf /config/app /app/config
 fi
-ln -sf /config/app /app/config
 
 RCLONE_CONFIG_DIR=$(dirname $(rclone config file | sed -n 2p))
 if [ ! -L "$RCLONE_CONFIG_DIR" ] && [ -d "$RCLONE_CONFIG_DIR" ]; then
     rm -rf "$RCLONE_CONFIG_DIR"
+    ln -sf /config/rclone "$RCLONE_CONFIG_DIR"
 fi
-ln -sf /config/rclone "$RCLONE_CONFIG_DIR"
 
 echo "[INFO] Executing "$@""
 
