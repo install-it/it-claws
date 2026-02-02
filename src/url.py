5c1c2d0533a39d108bdb580afa54ea46d2a72696
markmybytes <markmybytes@gmail.com>
2026-02-02 12:32:37 +0800

    Adjust waiting time

--- src/url.py
+++ src/url.py
@@ -83,10 +83,9 @@ def msi(remote: webdriver.Remote, url: str, dri_type: str, dri_name: str) -> str
         str: Direct download URL for the driver.
     """
     remote.get(url)
-
     try:
+        time.sleep(1)
         # close cookie consent overlay
-        time.sleep(0.5)
         remote.execute_script(
             'window.scrollTo({ top: document.body.scrollHeight / 3, behavior: "smooth" })')
         time.sleep(0.5)
